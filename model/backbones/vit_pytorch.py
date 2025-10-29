2ab9afb26b1a3981b45c4a7d4df92cb878041159
HoudaMenighed <menighedhouda1@gmail.com>
2025-10-29 18:22:13 +0100

    changed

--- model/backbones/vit_pytorch.py
+++ model/backbones/vit_pytorch.py
@@ -434,7 +434,7 @@ def vit_base_patch16_224_TransReID(img_size=(256, 128), stride_size=16, drop_rat
     model = TransReID(
         img_size=img_size, patch_size=16, stride_size=stride_size, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,\
         camera=camera, view=view, drop_path_rate=drop_path_rate, drop_rate=drop_rate, attn_drop_rate=attn_drop_rate,
-        norm_layer=partial(nn.LayerNorm, eps=1e-6),  sie_xishu=sie_xishu, local_feature=local_feature, **kwargs)
+        norm_layer=partial(nn.LayerNorm, eps=1e-6),   local_feature=local_feature, **kwargs)
 
     return model
 
